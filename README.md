27d384f6bae8019a4c4d6dcb87911c316c82a6a0
EDOHWARES <edohemmanuel4real@gmail.com>
2024-03-07 23:24:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Binary-Denary Converter
+Welcome to the Binary-Denary Converter project! This Python program allows you to convert numbers between binary and denary (decimal) representations, as well as denary to binary and vice versa.
+
+## Features
+- Convert binary numbers to denary (decimal) numbers.
+- Convert denary (decimal) numbers to binary numbers.
+- Simple and intuitive command-line interface
+
+## Getting Started
+To get started with the Binary-Denary Converter, follow these steps:
+
+- Clone the repository to your local machine:
+
+
+git clone https://github.com/EDOHWARES/base-converter-binary-and-denary-.git
+
+Navigate to the project directory:
+
+
+- cd binary-decimal converter
+
+- Run the program:
+
+python3 main.py
+
+## Usage
+- Once the program is running, you'll be prompted input the digits to convert:
+- You'll also be prompted to enter the base to convert to
+
+## Requirements
+Python 3.x
+## Contribution
+Contributions are welcome! If you'd like to contribute to the project, please fork the repository and submit a pull request with your changes.
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+## Acknowledgments
+Thank you for using the Binary-Denary Converter!
+
+Feel free to reach out to the project maintainer with any questions or feedback.
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,63 @@
+def bin_to_dec(binary_no):
+    dec = 0
+    b = str(binary_no)
+    for val in b:
+        if int(val) > 1:
+            return "Invalid Binary Digit!"
+    h_index = len(b) - 1
+
+    for char in b:
+        dec += int(char) * (2 ** h_index)
+        h_index -= 1
+
+    return dec
+
+
+def dec_to_bin(dec):
+    b = dec
+    remainders = []
+    while b != 0:
+        remainders.append(b % 2)
+        b = b // 2
+    remainders = remainders[::-1]
+    binary = int("".join(map(str, remainders)))
+
+    return binary
+
+
+def main():
+    print("""
+    A program that converts Binary to Denary and vice-versa
+    Input your base number!
+    """)
+
+    number = 0
+    base = ""
+
+    while number < 1:
+        try:
+            number = int(input("Input the number to convert: "))
+        except ValueError:
+            print("Number cannot be a non-digit!")
+
+        if number < 1:
+            print("Input a valid number!")
+        else:
+            break
+
+    while base != "bin" and base != "dec":
+        base = input("To what base? (bin/dec): ").lower()
+
+        if base != "bin" and base != "dec":
+            print("Input has to be either of 'bin' or 'dec'!")
+
+    if base == 'bin':
+        result = dec_to_bin(number)
+    elif base == 'dec':
+        result = bin_to_dec(number)
+
+    return f"{number} in {base}: {result}"
+
+print(main())
+
+
